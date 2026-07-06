cdeb35bd38b3f1a18f709f796fa4b89e008116e9
Lori Karikari <lori.karikari@gmail.com>
2026-07-06 21:01:48 +0200

    fix: clarify completion on Windows

--- internal/cli/completion_command.go
+++ internal/cli/completion_command.go
@@ -1,6 +1,9 @@
 package cli
 
 import (
+	"errors"
+	"runtime"
+
 	"github.com/alecthomas/kong"
 	"github.com/willabides/kongplete"
 )
@@ -8,5 +11,15 @@ import (
 type CompletionCmd struct{}
 
 func (c *CompletionCmd) BeforeApply(ctx *kong.Context) error {
+	if err := completionPlatformError(runtime.GOOS); err != nil {
+		return err
+	}
 	return (&kongplete.InstallCompletions{}).BeforeApply(ctx)
+}
+
+func completionPlatformError(goos string) error {
+	if goos == "windows" {
+		return errors.New("shell completion generation is not supported on Windows yet; use `azkit shell-init pwsh` for PowerShell shell integration")
+	}
+	return nil
 }
